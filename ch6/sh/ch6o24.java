afce3bd22e3b5feafdbfaf0565d66b22d42484e8
03LSH <LSHrPwud@gmail.com>
2022-03-02 10:15:54 +0900

    LSH ch6

---
+++ ch6/sh/ch6o24.java
@@ -0,0 +1,14 @@
+
+public class ch6o24 {
+  public static int abs(int value){
+    value = value >= 0 ? value : -value;
+    return value;
+}
+  public static void main(String[] args) {
+    int value = 5;
+    System.out.println(value+" 의 절대값:"+abs(value));
+    value = -10;
+    System.out.println(value+" 의 절대값:"+abs(value));
+  }
+
+}
---
+++ ch6/sh/ch6o3.java
@@ -0,0 +1,13 @@
+class Student {
+  String name;
+  int ban;
+  int no;
+  int kor;
+  int eng;
+  int math;
+}
+
+
+public class ch6o3 {
+
+}
