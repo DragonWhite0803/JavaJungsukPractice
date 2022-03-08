82b01b0dc59cf7092b3b87df7e003ccbf8dd691c
03LSH <LSHrPwud@gmail.com>
2022-03-08 10:17:07 +0900

    LSH ch8 commit

---
+++ ch8/sh/ch8o2.java
@@ -0,0 +1,13 @@
+
+public class ch8o2 {
+//  java.lang.ArithmeticException : / by zero
+//  at ExceptionEx18.method2(ExceptionEx18.java:12)
+//  at ExceptionEx18.method1(ExceptionEx18.java:8)
+//  at ExceptionEx18.main(ExceptionEx18.java:4)
+//  
+//  a. . 위의 내용으로 예외가 발생했을 당시 호출스택에 존재했던 메서드를 알 수 있다
+//  b. method2 , ExceptionEx18.java 12 . 예외가 발생한 위치는 메서드이며 파일의 12번째 줄이다
+//  c.발생한 예외는 ArithmeticException 이며 0으로 나누어서 예외가 발생했다
+//  d. method2 메서드가 method1 메서드를 호출하였고 그 위치는 ExceptionEx18.java 8 파일의 8번째 줄이다. --- method1이 method2를 호출한 것이다.
+  
+}
---
+++ ch8/sh/ch8o4.java
@@ -0,0 +1,16 @@
+
+public class ch8o4 {
+//  void method() throws InvalidNumberException, NotANumberException {}
+//  class NumberException extends RuntimeException {}
+//  class InvalidNumberException extends NumberException {}
+//  class NotANumberException extends NumberException {}
+//  
+//  a. try {method();} catch(Exception e) {}
+//  b. try {method();} catch(NumberException e) {} catch(Exception e) {} --- Exception는 최상예외처리이며 모든 예외처리들은 Exception 아래에 상속되어있다. 그러므로 답은 b
+//  c. try {method();} catch(Exception e) {} catch(NumberException e) {}
+//  d. try {method();} catch(InvalidNumberException e) {
+//  } catch(NotANumberException e) {}
+//  e. try {method();} catch(NumberException e) {}
+//  f. try {method();} catch(RuntimeException e) {}
+  
+}
