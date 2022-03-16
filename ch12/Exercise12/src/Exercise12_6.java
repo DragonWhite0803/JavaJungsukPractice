6cd8400b31309481b430f441c8ab05d46f20b17a
AllenBaek <ehddnr418154@gmail.com>
2022-03-16 15:05:21 +0900

    no message

--- ch12/Exercise12/src/Exercise12_6.java
+++ ch12/Exercise12/src/Exercise12_6.java
@@ -3,7 +3,7 @@ public class Exercise12_6 {
 //	[12-6] 다음 중 메타 애너테이션이 아닌 것을 모두 고르시오.
 //	a. Documented
 //	b. Target
-//	c. Native
+//	c. Native -> Native '메서드'에서 참조되는 상수 앞에 붙힌다.
 //	d. Inherited
 
 }
